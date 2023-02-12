3a8c5f6e82a29f420f85a910ede2683f5d807b89
James <N3rdP1um23@users.noreply.github.com>
2023-02-12 15:25:55 -0500

    Update __init__.py
    Upgraded calls to older deprecated functions
    - `async_setup_platforms` => `async_forward_entry_setup`
    - `async_unload_platforms` => `async_forward_entry_unload`

--- custom_components/omnilogic/__init__.py
+++ custom_components/omnilogic/__init__.py
@@ -59,14 +59,14 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         OMNI_API: api,
     }
 
-    hass.config_entries.async_setup_platforms(entry, PLATFORMS)
+    hass.config_entries.async_forward_entry_setup(entry, PLATFORMS)
 
     return True
 
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
-    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+    unload_ok = await hass.config_entries.async_forward_entry_unload(entry, PLATFORMS)
     if unload_ok:
         hass.data[DOMAIN].pop(entry.entry_id)
 
